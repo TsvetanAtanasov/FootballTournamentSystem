b2556f3bb3c0d4cb1c93bcd2baf3aad47dea3f86
Tsvetan Atanasov <tsvetan.atanasov@negometrix.bg>
2021-07-19 17:56:03 +0300

    add common infrastructure project

---
+++ Common.Infrastructure/Persistence/DataRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Common.Infrastructure.Persistence
+{
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Common.Domain;
+
+    internal abstract class DataRepository<TDbContext, TEntity> : IDomainRepository<TEntity>
+        where TDbContext : IDbContext
+        where TEntity : class, IAggregateRoot
+    {
+        protected DataRepository(TDbContext db) => this.Data = db;
+
+        protected TDbContext Data { get; }
+
+        protected IQueryable<TEntity> All() => this.Data.Set<TEntity>();
+
+        public async Task Save(
+            TEntity entity,
+            CancellationToken cancellationToken = default)
+        {
+            this.Data.Update(entity);
+
+            await this.Data.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
